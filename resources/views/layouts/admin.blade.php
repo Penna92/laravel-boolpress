000aeec609ee91b40c39a9df3dd8064369fdc983
Penna92 <giacomopenna92@gmail.com>
2022-06-24 16:40:01 +0200

    Aggiunti link nella navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,7 +30,16 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Boolpress
+                    Home
+                </a>
+                <a class="navbar-brand" href="{{ url('/admin/posts') }}">
+                    Posts
+                </a>
+                <a class="navbar-brand" href="{{ url('/admin/categories') }}">
+                    Categories
+                </a>
+                <a class="navbar-brand" href="{{ url('/admin/tags') }}">
+                    Tags
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
